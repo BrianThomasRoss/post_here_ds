4738b417eb5d13ccf2b501cfb17c473940051644
brianthomasross <brianthomasross@users.noreply.github.com>
2020-01-13 16:54:22 -0500

    Add ingestion namespace to blueprint

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -4,6 +4,7 @@ from flask_restplus import Api
 from flask import Blueprint
 
 from .main.controller.prediction_controller import api as pred_ns
+from .main.controller.ingestion_controller import api as ingest_ns
 from .subreddit_classifier.models import load_mvp
 
 blueprint = Blueprint('api', __name__)
@@ -15,5 +16,6 @@ api = Api(blueprint,
           )
 
 api.add_namespace(pred_ns, path='/predict')
+api.add_namespace(ingest_ns, path='/ingestion')
 
 
